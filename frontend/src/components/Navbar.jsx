41ef7883d4c215d04ed1123cb53b7449da9130a1
Aknniyet <akniet0512@gmail.com>
2026-03-16 09:53:31 +0500

    Initial commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import "../styles/navbar.css";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -7,9 +8,17 @@ function Navbar() {
         <div className="logo">BuddyConnect</div>
 
         <nav className="nav-links">
-          <a href="/">Home</a>
-          <a href="/about">About</a>
-          <a href="/guide">Adaptation Guide</a>
+          <NavLink to="/"  className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}> 
+           Home 
+          </NavLink>
+
+          <NavLink to="/about" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+           About
+          </NavLink>
+
+            <NavLink to="/guide" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}> 
+            Adaptation Guide 
+            </NavLink>
         </nav>
 
         <div className="nav-actions">
